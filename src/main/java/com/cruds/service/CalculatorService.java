37ab5b84b3921b832d897578eb9c936cfd6eda6c
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2024-06-06 11:05:08 +0530

    Update CalculatorService.java

--- src/main/java/com/cruds/service/CalculatorService.java
+++ src/main/java/com/cruds/service/CalculatorService.java
@@ -1,11 +1,11 @@
 package com.cruds.service;
 
+// Ignore all issues in this method
+@SuppressWarnings("squid:S1118")
 public class CalculatorService {
-    // Ignore all issues in this method
-    @SuppressWarnings("squid:S1118")
+     // Issue: This statement has no effect
 	public static int add(int x, int y)
 	{
-	    // Issue: This statement has no effect
 		return x + y; // This statement is just an example and will be ignored by SonarQube
 	}
   
